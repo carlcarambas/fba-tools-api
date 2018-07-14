04467355f5bfa1407a3d74edba5aca00feb9ea4c
EJ Mercado <ej@ejmercado.com>
2018-07-15 00:24:42 +0800

    modified implementation to add support for column name mapping

--- daos/raw-query-runner.js
+++ daos/raw-query-runner.js
@@ -1,16 +1,21 @@
 /**
+ * @todo Modify current select view method to return array of column named "name" at index 1 or return statement
+ */
+
+
+/**
  * 
  * @param {*} query String. Raw SQL query  to be run 
  * @param {*} parameters Array. Parameters to be prepared with query (if needed)
  * @param {*} knex Object. Knex connection
  */
-const runRawQuery = function(query, parameters, knex){
+const runRawQuery = function (query, parameters, knex) {
     return knex.raw(query, parameters).
-            then(data => {
-                let res = JSON.stringify(data);
-                let temp = JSON.parse(res);
-                return Promise.resolve(temp);
-            });
+        then(data => {
+            let res = JSON.stringify(data);
+            let temp = JSON.parse(res);
+            return Promise.resolve(temp);
+        });
 };
 
 /**
@@ -19,7 +24,7 @@ const runRawQuery = function(query, parameters, knex){
  * @param {*} parameters 
  * @param {*} sequelize 
  */
-const runRawQuerySequelize = function(query, parameters, sequelize){
+const runRawQuerySequelize = function (query, parameters, sequelize) {
     return sequelize.query(query);
 };
 
@@ -29,13 +34,21 @@ const runRawQuerySequelize = function(query, parameters, sequelize){
  * @param {*} database
  * @description Return all tables (includes views) 
  */
-const getListOfTables = function (knex, database){
+const getListOfTables = function (knex, database) {
     const query = `SHOW FULL TABLES IN ??`;
     return knex.raw(query, [database]);
 }
 
 const selectView = function (knex, view) {
-    return knex.select().table(view);
+    const query = `SELECT * FROM ??`;
+    const parameters = [view];
+
+    return knex.raw(query, parameters).
+        then(data => {
+            let res = JSON.stringify(data);
+            let temp = JSON.parse(res);
+            return Promise.resolve(temp);
+        });
 }
 
 module.exports = {
